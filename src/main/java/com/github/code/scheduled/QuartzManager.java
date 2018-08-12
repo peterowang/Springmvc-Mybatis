1a5d4fa0a6a4093fc6bbaacba667f47679409528
peterowang <ilovehadoop@sina.com>
2018-08-12 12:03:00 +0800

    init dynamic job and static job quartz

--- src/main/java/com/github/code/scheduled/QuartzManager.java
+++ src/main/java/com/github/code/scheduled/QuartzManager.java
@@ -33,7 +33,6 @@ public class QuartzManager {
             Scheduler sched = schedulerFactory.getScheduler();
             // 任务名，任务组，任务执行类
             JobDetail jobDetail= JobBuilder.newJob(jobClass).withIdentity(jobName, jobGroupName).build();
-
             // 触发器
             TriggerBuilder<Trigger> triggerBuilder = TriggerBuilder.newTrigger();
             // 触发器名,触发器组
@@ -43,10 +42,8 @@ public class QuartzManager {
             triggerBuilder.withSchedule(CronScheduleBuilder.cronSchedule(cron));
             // 创建Trigger对象
             CronTrigger trigger = (CronTrigger) triggerBuilder.build();
-
             // 调度容器设置JobDetail和Trigger
             sched.scheduleJob(jobDetail, trigger);
-
             // 启动
             if (!sched.isShutdown()) {
                 sched.start();
@@ -57,16 +54,14 @@ public class QuartzManager {
     }
 
     /**
-     * @Description: 修改一个任务的触发时间
-     *
      * @param jobName
      * @param jobGroupName
-     * @param triggerName 触发器名
+     * @param triggerName      触发器名
      * @param triggerGroupName 触发器组名
-     * @param cron   时间设置，参考quartz说明文档
+     * @param cron             时间设置，参考quartz说明文档
+     * @Description: 修改一个任务的触发时间
      */
-    public static void modifyJobTime(String jobName,
-                                     String jobGroupName, String triggerName, String triggerGroupName, String cron) {
+    public static void modifyJobTime(String jobName, String jobGroupName, String triggerName, String triggerGroupName, String cron) {
         try {
             Scheduler sched = schedulerFactory.getScheduler();
             TriggerKey triggerKey = TriggerKey.triggerKey(triggerName, triggerGroupName);
@@ -74,7 +69,6 @@ public class QuartzManager {
             if (trigger == null) {
                 return;
             }
-
             String oldTime = trigger.getCronExpression();
             if (!oldTime.equalsIgnoreCase(cron)) {
                 /** 方式一 ：调用 rescheduleJob 开始 */
@@ -90,12 +84,11 @@ public class QuartzManager {
                 // 方式一 ：修改一个任务的触发时间
                 sched.rescheduleJob(triggerKey, trigger);
                 /** 方式一 ：调用 rescheduleJob 结束 */
-
                 /** 方式二：先删除，然后在创建一个新的Job  */
-                //JobDetail jobDetail = sched.getJobDetail(JobKey.jobKey(jobName, jobGroupName));
-                //Class<? extends Job> jobClass = jobDetail.getJobClass();
-                //removeJob(jobName, jobGroupName, triggerName, triggerGroupName);
-                //addJob(jobName, jobGroupName, triggerName, triggerGroupName, jobClass, cron);
+                /*JobDetail jobDetail = sched.getJobDetail(JobKey.jobKey(jobName, jobGroupName));
+                Class<? extends Job> jobClass = jobDetail.getJobClass();
+                removeJob(jobName, jobGroupName, triggerName, triggerGroupName);
+                addJob(jobName, jobGroupName, triggerName, triggerGroupName, jobClass, cron);*/
                 /** 方式二 ：先删除，然后在创建一个新的Job */
             }
         } catch (Exception e) {
@@ -117,10 +110,12 @@ public class QuartzManager {
             Scheduler sched = schedulerFactory.getScheduler();
 
             TriggerKey triggerKey = TriggerKey.triggerKey(triggerName, triggerGroupName);
-
-            sched.pauseTrigger(triggerKey);// 停止触发器
-            sched.unscheduleJob(triggerKey);// 移除触发器
-            sched.deleteJob(JobKey.jobKey(jobName, jobGroupName));// 删除任务
+            // 停止触发器
+            sched.pauseTrigger(triggerKey);
+            // 移除触发器
+            sched.unscheduleJob(triggerKey);
+            // 删除任务
+            sched.deleteJob(JobKey.jobKey(jobName, jobGroupName));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
